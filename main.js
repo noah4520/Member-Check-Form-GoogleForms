6d68344dc36058606f35b9d5b0b0b22e138e8e4a
jasonchen <jason316497@gmail.com>
2023-01-27 23:51:27 +0800

    Feat: 整理現有功能
    1. 合併自行出入營的資料
    2. 移除計算總人數的資料
    3. 調整休假與收假區塊為不可排序
    4. 調整變數名稱

--- main.js
+++ main.js
@@ -16,50 +16,32 @@ let outPutKeyText = "";
 let groupByKeyAry = [];
 
 window.onload = (event) => {
+
   new ClipboardJS('#CarTextBtn');
-  new ClipboardJS('#SelfTextBtn');
-  main();
-}
-/*
-const select = (DOM) => document.querySelector(DOM);
+  // new ClipboardJS('#SelfTextBtn');
 
-select('#CarTextBtn').addEventListener('click', () => {
-  select('#CarTextarea').select();
-  document.execCommand("copy");
-  alert("複製成功！");
-})
-
-select('#SelfTextBtn').addEventListener('click', () => {
-  select('#SelfTextarea').select();
-  document.execCommand("copy");
-  alert("複製成功！");
-})
-*/
-function main() {
-
-  new Sortable(document.getElementById("Car-LeaveCamp-List"), {
+  new Sortable(document.getElementById("LeaveCampList"), {
     filter: "#ListTitle",
     ghostClass: 'blue-background-class',
     animation: 150,
   });
 
-  new Sortable(document.getElementById("Car-BackCamp-List"), {
+  new Sortable(document.getElementById("BackCampList"), {
     ghostClass: 'blue-background-class',
     animation: 150,
   });
 
-  new Sortable(document.getElementById("SelfText"), {
-    ghostClass: 'blue-background-class',
-    animation: 150,
-  });
+  main();
+}
+
+function main() {
 
   const inputFile = document.querySelector("input[type='file']");
 
   inputFile.onchange = (event) => {
 
-    document.getElementById("Car-LeaveCamp-List").innerText = "";
-    document.getElementById("Car-BackCamp-List").innerText = "";
-    document.getElementById("SelfText").innerText = "";
+    document.getElementById("LeaveCampList").innerText = "";
+    document.getElementById("BackCampList").innerText = "";
 
     // 取得要輸出的key
     outPutKeyText = document.getElementById("IdKeyInput").value;
@@ -136,165 +118,59 @@ function outputJSON(data) {
   let startDate = document.getElementById("startDate").value.slice(5).replace('-', '/');
   let endDate = document.getElementById("endDate").value.slice(5).replace('-', '/');
 
-  /*
-  ●${startDate} 休假，總人數${goKeelungArray.length + goTaoyuanArray.length + goMiaoliArray.length + goTaichungArray.length}人
-  
-  1.基隆：${goKeelungArray.length}員
-  
-    學號如下：${goKeelungArray.join("、")}
-  
-  2.桃園：${goTaoyuanArray.length}員
-  
-    學號如下：${goTaoyuanArray.join("、")}
-  
-  3.苗栗：${goMiaoliArray.length}員
-  
-    學號如下：${goMiaoliArray.join("、")}
-  
-  4.臺中：${goTaichungArray.length}員 
-  
-    學號如下：${goTaichungArray.join("、")}
-  
-  
-  ●${endDate} 收假，總人數${fromKeelungArray.length + fromTaoyuanArray.length + fromMiaoliArray.length + fromTaichungArray.length}人
-  
-  1.基隆：${fromKeelungArray.length}員
-  
-    學號如下：${fromKeelungArray.join("、")}
-  
-  2.桃園：${fromTaoyuanArray.length}員
-  
-    學號如下：${fromTaoyuanArray.join("、")}
-  
-  3.苗栗：${fromMiaoliArray.length}員
-  
-    學號如下：${fromMiaoliArray.join("、")}
-  
-  4.臺中：${fromTaichungArray.length}員
-  
-    學號如下：${fromTaichungArray.join("、")}`
-  
-  
-  let selfText = `自行出營：${goSelfArray.length}員
-  
-  學號如下：${goSelfArray.join("、")}
-  
-  
-  自行入營：${fromSelfArray.length}員
-  
-  學號如下：${fromSelfArray.join("、")}
-  `
-  */
-
   let carText =
     `${document.getElementById("battalion").value}：
 
-●${startDate} 休假，總人數${data[0].memberCount}人\n\n`;
+●${startDate} 休假\n\n`;
 
-  createDivList(carText, "Car-LeaveCamp");
+  document.getElementById("LeaveListTitle").innerText = carText;
 
-  let selfText = "";
-  let listNum = 1;
+  // data[0]對應出營方式
+  data[0].forEach(data => {
 
-  // data[0]對應輸出分組順序1
-  data[0]["data"].forEach(data => {
-    if (data["place"].slice(0, 2) === "不搭") {
-      selfText =
-        `自行出營：${data["id"].length}員
-
-學號如下：${data["id"].sort().join("、")}\n`;
-      createDivList(selfText, "self");
-      return;
-    }
-    else {
-      carText =
-        `${data["place"].slice(0, 2)}： ${data["id"].length}員
+    carText =
+      `${data["place"]}： ${data["id"].length}員
             
-學號如下：${data["id"].sort().join("、")}\n\n`;
-      createDivList(carText, "Car-LeaveCamp");
-    }
+    學號如下：${data["id"].sort().join("、")}\n\n`;
+    createDivList(carText, "LeaveCamp");
   });
 
-  carText = `\n●${endDate} 收假，總人數${data[1].memberCount}人\n\n`;
-  createDivList(carText, "Car-BackCamp");
+  carText = `\n●${endDate} 收假\n\n`;
+  document.getElementById("BackListTitle").innerText = carText;
 
-  listNum = 1;
-
-  // data[1]對應輸出分組順序2
-  data[1]["data"].forEach(data => {
-    if (data["place"].slice(0, 2) === "不搭") {
-      selfText =
-        `\n自行入營：${data["id"].length}員
-
-學號如下：${data["id"].sort().join("、")}\n\n`;
-      createDivList(selfText, "self");
-      return;
-    }
-    else {
-      carText =
-        `${data["place"].slice(0, 2)}： ${data["id"].length}員
+  // data[1]對應回營方式
+  data[1].forEach(data => {
+    carText =
+      `${data["place"]}： ${data["id"].length}員
             
-學號如下：${data["id"].sort().join("、")}\n`;
-      createDivList(carText, "Car-BackCamp");
-    }
+    學號如下：${data["id"].sort().join("、")}\n\n`;
+    createDivList(carText, "BackCamp");
   });
-
-  // document.getElementById("CarTextarea").value = carText;
-  // document.getElementById("SelfTextarea").value = selfText;
 
   document.getElementById("notInList").innerText = notFillOutArray.join("、");
 }
-
-/*
-function addPlaceArrayFunc(data, placeText, addType) {
- 
-  let typeText = "";
- 
-  if (addType === 1) {
-    typeText = "離營下車的地點";
-  }
-  if (addType === 2) {
-    typeText = "回營上車的地點";
-  }
- 
-  let tempAry = data.filter(data => data[typeText] === placeText).map(data => data[outPutKeyText]).sort();
-  return tempAry;
-}
-*/
 
 //建立新的可拉動 div 區塊
 function createDivList(content, addType) {
 
   let typeText = "";
 
-  /*
-  if (addType === "car") {
-    typeText = "CarText";
-  }
-  if (addType === "self") {
-    typeText = "SelfText";
-  }
-  */
-
   switch (addType) {
-    case 'Car-LeaveCamp': {
-      typeText = "Car-LeaveCamp-List";
+    case 'LeaveCamp': {
+      typeText = "LeaveCampList";
       break;
     }
-    case 'Car-BackCamp': {
-      typeText = "Car-BackCamp-List";
-      break;
-    }
-    case 'self': {
-      typeText = "SelfText";
+    case 'BackCamp': {
+      typeText = "BackCampList";
       break;
     }
   }
-  const div = document.createElement("div");
-  div.className = ("list-item");
-  div.style = "white-space: pre-wrap;";
-  div.append(content);
-  document.getElementById(typeText).appendChild(div);
+
+  const li = document.createElement("li");
+  li.className = ("list-item");
+  li.style = "white-space: pre-wrap;";
+  li.append(content);
+  document.getElementById(typeText).appendChild(li);
 }
 
 function outputArrayFunc(data) {
@@ -303,7 +179,7 @@ function outputArrayFunc(data) {
   let tempObj = {};
   // 分別對不同的輸出分組順序進行分組
   for (key in groupByKeyAry) {
-    let memberCount = 0;
+
     tempObj = (data.map(data => ({ place: data[groupByKeyAry[key]], id: data[outPutKeyText] }))
       .reduce((groupByPlace, data) => {
 
@@ -320,18 +196,7 @@ function outputArrayFunc(data) {
         }
         return groupByPlace;
       }, []));
-
-    //計算除了自行出入以外的總人數
-    tempObj.forEach(data => {
-
-      if (data["place"].slice(0, 2) === "不搭") {
-        return;
-      }
-      else {
-        memberCount += data["id"].length;
-      }
-    });
-    campAry.push({ data: tempObj, memberCount: memberCount });
+    campAry.push(tempObj);
   }
 
   return campAry;
